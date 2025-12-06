1159d4ab6f7f8d2b6852860bc958ea9e3cc0e847
Lucas da Luz Morais <llucaslluz@gmail.com>
2025-12-06 10:38:43 -0300

    criando validaçoes para deixar o programa mais seguro

--- bin/calculadora.dart
+++ bin/calculadora.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
-
 void main() {
   double numeroUm = 0;
   double numeroDois = 0;
   String operacao = "";
+  List<String> operacoes = <String>["+", "-", "*", "/"];
+  String? entrada = "";
 
   void soma() {
     print(numeroUm + numeroDois);
@@ -38,33 +39,38 @@ void main() {
   }
 }
 
-  print("Digite o Primeiro valor:");
-
-  String? entrada = stdin.readLineSync();
-  if(entrada != null) {
-    if (entrada != "") {
-      numeroUm = double.parse(entrada);
+  void getOperacao() {
+    print("Digite uma operação:");
+    entrada = stdin.readLineSync();
+    if (entrada != null) {
+      if (operacoes.contains(entrada!)) {
+        operacao = entrada!;
+      }
     }
   }
 
-  print("Digite uma Operação matematica:");
+  print("Digite o Primeiro valor:");
 
-    entrada = stdin.readLineSync();
-  if (entrada != null) {
-    operacao = entrada;
+  entrada = stdin.readLineSync();
+  if(entrada != null) {
+    if (entrada != "") {
+      numeroUm = double.parse(entrada!);
+    }
   }
+
+  getOperacao();
 
   print("Digite o Segundo valor:");
 
   entrada = stdin.readLineSync();
   if (entrada != null) {
     if (entrada != "") {
-      numeroDois = double.parse(entrada);
+      numeroDois = double.parse(entrada!);
     }
   }
 
   print("O Resultado da operação é:");
-  
+
   calcular();
 
 
